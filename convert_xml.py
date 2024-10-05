f6121d3ac46e095c63bc40157da10398408e5651
sangi pc <sangipc29@gmail.com>
2024-10-05 17:37:35 +0200

    Initial commit

---
+++ convert_xml.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size/width').text),
+                     int(root.find('size/height').text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+path_to_xml = '/Users/sangi/Documents/pothole_proj/data/images_xml'
+xml_df = xml_to_csv(path_to_xml)
+xml_df.to_csv('pothole_labels.csv', index=False)
